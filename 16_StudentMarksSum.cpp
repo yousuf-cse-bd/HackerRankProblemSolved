624c8c2345a63179de73db75889d6d2d10863200
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-07-17 09:43:32 +0600

    Summation of marks gender wise.

---
+++ 16_StudentMarksSum.cpp
@@ -0,0 +1,59 @@
+/**
+ * @file 16_StudentMarksSum.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Summation of marks gender wise.
+ * @version 0.1
+ * @date 2022-07-17
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+//Complete the following function.
+
+int marks_summation(int* marks, int number_of_students, char gender) {
+  //Write your code here.
+  unsigned int sum = 0;
+  if(gender == 'b')
+    {
+        for(int student = 0; student < number_of_students; student += 2)
+        {
+            sum += (marks[student]);
+            // printf("%d\n",marks[student]);
+
+        }
+    }
+    if(gender == 'g')
+    {
+        for(int student = 1; student < number_of_students; student += 2)
+        {
+            sum += (marks[student]);
+            // printf("%d\n",marks[student]);
+        }
+    }
+    return sum;
+}
+
+int main() {
+    int number_of_students;
+    char gender;
+    int sum;
+  
+    scanf("%d", &number_of_students);
+    int *marks = (int *) malloc(number_of_students * sizeof (int));
+ 
+    for (int student = 0; student < number_of_students; student++) {
+        scanf("%d", (marks + student));
+    }
+    
+    scanf(" %c", &gender);
+    sum = marks_summation(marks, number_of_students, gender);
+    printf("%d", sum);
+    free(marks);
+ 
+    return 0;
+}
